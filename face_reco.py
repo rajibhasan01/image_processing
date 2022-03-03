76d3a4b5444b8841ac4cc28870bf4b8ecccb5ad1
rajibhasan01 <hasan.rajib1996@gmail.com>
2022-03-03 19:09:10 +0600

    updated

--- face_reco.py
+++ face_reco.py
@@ -9,9 +9,10 @@ img_encoding = face_recognition.face_encodings(rgb_img)[0];
 img2 = cv2.imread("test/rajib_4.jpg");
 rgb_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB);
 img_encoding2 = face_recognition.face_encodings(rgb_img2)[0];
-
-
 result = face_recognition.compare_faces([img_encoding], img_encoding2);
+
+
+
 
 print("Result: ", result[0]);
 
